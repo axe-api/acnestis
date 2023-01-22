623f3d92c93f48f4642c73af450b3d047a947ea3
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-01-22 14:13:02 +0100

    Fixed .env variables

--- src/services/db.js
+++ src/services/db.js
@@ -1,11 +1,11 @@
 const db = require("knex")({
-  client: "mysql",
+  client: process.env.DB_CLIENT,
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
-    password: "12345678",
-    database: "xblog",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   },
 });
 
